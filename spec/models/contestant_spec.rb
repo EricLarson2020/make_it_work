6b7bf99cd45cb75c7a8471c39373cbbeb46e926a
Eric Larson <ericlarsonbroom@gmail.com>
2020-05-15 09:38:34 -0600

    Created model test for project names

--- spec/models/contestant_spec.rb
+++ spec/models/contestant_spec.rb
@@ -13,4 +13,17 @@ RSpec.describe Contestant, type: :model do
     it {should have_many :contestant_projects}
     it {should have_many(:projects).through(:contestant_projects)}
   end
+
+  describe "methods" do
+    it "project_names" do
+      recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+      furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+      gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+      news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+      upholstery_tux = furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+      ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+      ContestantProject.create(contestant_id: gretchen.id, project_id: upholstery_tux.id)
+      expect(gretchen.project_names).to eq("News Chic, Upholstery Tuxedo")
+    end
+  end
 end
