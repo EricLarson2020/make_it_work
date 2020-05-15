2f01198764640f9ff6e98d64e71f15b20614f263
Eric Larson <ericlarsonbroom@gmail.com>
2020-05-15 10:28:42 -0600

    created test for add contestant to project

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -78,6 +78,44 @@ RSpec.describe "Project Show Page", type: :feature do
 
 
   end
+
+  it "can create a new contestant" do
+    recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+    furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+
+    news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+    boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+
+    upholstery_tux = furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+    lit_fit = furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+
+    jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+    kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+    erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+
+
+    ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+    ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+    ContestantProject.create(contestant_id: gretchen.id, project_id: upholstery_tux.id)
+    ContestantProject.create(contestant_id: kentaro.id, project_id: upholstery_tux.id)
+    ContestantProject.create(contestant_id: kentaro.id, project_id: boardfit.id)
+    ContestantProject.create(contestant_id: erin.id, project_id: boardfit.id)
+
+    visit "/projects/#{boardfit.id}"
+    fill_in :name, with: "Bob"
+    fill_in :age, with: 20
+    fill_in :hometown, with: "Denver"
+    fill_in :years_of_experience, with: 10
+    click_button "Add Contestant To Project"
+    expect(current_path).to eql("/projects/#{boardfit.id}")
+    expect(page).to have_content("Number of Contestants: 3")
+    visit "/contestants"
+
+    expect(page).to have_content("Bob")
+    expect(page).to have_content("boardfit")
+
+  end
 end
 # Average Contestant Experience: 10.25 years)
 
